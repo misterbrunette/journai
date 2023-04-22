cfa695c4b95cb20e666e2a6e575c1b53e866abd6
misterbrunette <131505434+misterbrunette@users.noreply.github.com>
2023-04-22 16:23:28 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,157 +1,74 @@
-// Import the functions you need from the SDKs you need
-import { auth, db } from './firebase-config.js';
-import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
-import { collection, addDoc, getDocs, query, orderBy, where, onSnapshot } from 'firebase/firestore';
+import { auth, firestore } from "./firebase-config.js";
 
-// DOM elements
-const signInLink = document.getElementById('sign-in-link');
-const signUpLink = document.getElementById('sign-up-link');
-const signOutLink = document.getElementById('sign-out-link');
-const signInModal = document.getElementById('sign-in-modal');
-const signUpModal = document.getElementById('sign-up-modal');
-const signInForm = document.getElementById('sign-in-form');
-const signUpForm = document.getElementById('sign-up-form');
-const signInError = document.getElementById('auth-error');
-const signUpError = document.getElementById('auth-error');
-const closeModalButtons = document.getElementsByClassName('close');
-const userBanner = document.getElementById('user-banner');
-const userEmailDisplay = document.getElementById('user-email');
-const signOutButton = document.getElementById('sign-out');
-const journalForm = document.getElementById('journal-form');
-const journalTable = document.getElementById('journal-entries-table');
-const titleInput = document.getElementById('title');
-const contentInput = document.getElementById('content');
-const saveButton = document.getElementById('save-button');
-const feedbackForm = document.getElementById('feedback-form');
-const feedbackText = document.getElementById('feedback-text');
+const signInLink = document.getElementById("sign-in-link");
+const signUpLink = document.getElementById("sign-up-link");
+const signInModal = document.getElementById("sign-in-modal");
+const signUpModal = document.getElementById("sign-up-modal");
+const signInForm = document.getElementById("sign-in-form");
+const signUpForm = document.getElementById("sign-up-form");
+const userBanner = document.getElementById("user-banner");
+const userEmail = document.getElementById("user-email");
+const signOut = document.getElementById("sign-out");
 
-// Sign in and sign up modal handlers
-signInLink.addEventListener('click', () => {
-  signInModal.style.display = 'block';
-});
-
-signUpLink.addEventListener('click', () => {
-  signUpModal.style.display = 'block';
-});
-
-Array.from(closeModalButtons).forEach(button => {
-  button.addEventListener('click', () => {
-    signInModal.style.display = 'none';
-    signUpModal.style.display = 'none';
-  });
-});
-
-window.addEventListener('click', (event) => {
-  if (event.target === signInModal || event.target === signUpModal) {
-    signInModal.style.display = 'none';
-    signUpModal.style.display = 'none';
-  }
-});
-
-// Authentication handlers
-signInForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const email = document.getElementById('sign-in-email').value;
-  const password = document.getElementById('sign-in-password').value;
-
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    signInModal.style.display = 'none';
-  } catch (error) {
-    signInError.textContent = error.message;
-  }
-});
-
-signUpForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const email = document.getElementById('sign-up-email').value;
-  const password = document.getElementById
-
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    signUpModal.style.display = 'none';
-  } catch (error) {
-    signUpError.textContent = error.message;
-  }
-});
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    userBanner.style.display = 'block';
-    userEmailDisplay.textContent = user.email;
-    signOutButton.addEventListener('click', () => signOut(auth));
-  } else {
-    userBanner.style.display = 'none';
-  }
-});
-
-// Journal entry handlers
-journalForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const title = titleInput.value;
-  const content = contentInput.value;
-  const date = new Date().toISOString().substring(0, 10);
-  const userId = auth.currentUser.uid;
-
-  try {
-    await addDoc(collection(db, 'journalEntries'), {
-      title,
-      content,
-      date,
-      userId
-    });
-    titleInput.value = '';
-    contentInput.value = '';
-  } catch (error) {
-    console.error('Error adding journal entry:', error);
-  }
-});
-
-// Update UI
-const updateJournalEntriesUI = (querySnapshot) => {
-  const userId = auth.currentUser.uid;
-  const journalEntries = querySnapshot.docs.filter((doc) => doc.data().userId === userId);
-  journalTable.innerHTML = '';
-  journalEntries.forEach((doc) => {
-    const entry = doc.data();
-    const tableRow = document.createElement('tr');
-    tableRow.innerHTML = `
-      <td>${entry.date}</td>
-      <td>${entry.title}</td>
-      <td>${entry.content}</td>
-    `;
-    journalTable.appendChild(tableRow);
-  });
+signInLink.onclick = () => {
+  signInModal.style.display = "block";
 };
 
-onSnapshot(query(collection(db, 'journalEntries'), orderBy('date', 'desc')), updateJournalEntriesUI);
+signUpLink.onclick = () => {
+  signUpModal.style.display = "block";
+};
 
-// Send feedback to ChatGPT
-feedbackForm.addEventListener('submit', async (event) => {
+signInModal.onclick = (event) => {
+  if (event.target == signInModal) {
+    signInModal.style.display = "none";
+  }
+};
+
+signUpModal.onclick = (event) => {
+  if (event.target == signUpModal) {
+    signUpModal.style.display = "none";
+  }
+};
+
+signInForm.onsubmit = async (event) => {
   event.preventDefault();
-  const feedback = feedbackText.value;
-  if (!feedback) return;
+  const email = document.getElementById("sign-in-email").value;
+  const password = document.getElementById("sign-in-password").value;
 
-  // Replace 'YOUR_API_KEY' with your actual OpenAI API key
-  const apiKey = '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';
+  try {
+    await auth.signInWithEmailAndPassword(email, password);
+    signInModal.style.display = "none";
+  } catch (error) {
+    alert("Error signing in: " + error.message);
+  }
+};
 
-  const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
-    },
-    body: JSON.stringify({
-      prompt: feedback,
-      max_tokens: 50,
-      n: 1,
-      stop: null,
-      temperature: 0.5
-    })
-  });
+signUpForm.onsubmit = async (event) => {
+  event.preventDefault();
+  const email = document.getElementById("sign-up-email").value;
+  const password = document.getElementById("sign-up-password").value;
 
-  const data = await response.json();
-  const generatedResponse = data.choices[0].text;
-  alert(`Assistant's response: ${generatedResponse}`);
-  feedbackText.value = '';
+  try {
+    await auth.createUserWithEmailAndPassword(email, password);
+    signUpModal.style.display = "none";
+  } catch (error) {
+    alert("Error signing up: " + error.message);
+  }
+};
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    signInLink.style.display = "none";
+    signUpLink.style.display = "none";
+    userBanner.style.display = "block";
+    userEmail.textContent = user.email;
+  } else {
+    signInLink.style.display = "block";
+    signUpLink.style.display = "block";
+    userBanner.style.display = "none";
+  }
 });
+
+signOut.onclick = () => {
+  auth.signOut();
+};
--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from '/node_modules/firebase/app';
-import { getAuth } from '/node_modules/firebase/auth';
-import { getFirestore } from '/node_modules/firebase/firestore';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-app-compat.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth-compat.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore-compat.js";
 
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: "AIzaSyD1PqjsmOknKl2ZuRUORLG-4faiYFiuKA0",
   authDomain: "journai-6b1fe.firebaseapp.com",
+  databaseURL: "https://journai-6b1fe-default-rtdb.firebaseio.com",
   projectId: "journai-6b1fe",
   storageBucket: "journai-6b1fe.appspot.com",
   messagingSenderId: "13426463829",
   appId: "1:13426463829:web:fdf1588552513c567f70dc",
   measurementId: "G-60CFJTY6S0"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore();
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
-export { auth, db };
+export { auth, firestore };
